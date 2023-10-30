a936b973322fd3bce357f43f5cf22b1d8257f9aa
cnouguier <ch.nouguier@gmail.com>
2023-10-30 16:18:20 +0100

    refactor: use own s3 service chore: added chunk/object properties in responses

--- test/test.js
+++ test/test.js
@@ -18,22 +18,22 @@ let expressServer
 let inputId
 let outputIds = []
 
-const s3Options = {
-  s3Client: {
-    credentials: {
-      accessKeyId: process.env.S3_ACCESS_KEY_ID,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
-    },
-    endpoint: process.env.S3_ENDPOINT,
-    region: process.env.S3_REGION,
-    signatureVersion: 'v4'
-  },
-  bucket: process.env.S3_BUCKET
-}
-
 const options = {
   s3ServicePath: 'path-to-s3',
-  workingDir: './test/tmp'
+  workingDir: './test/tmp',
+  s3Options: {
+    s3Client: {
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY_ID,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+      },
+      endpoint: process.env.S3_ENDPOINT,
+      region: process.env.S3_REGION,
+      signatureVersion: 'v4'
+    },
+    bucket: process.env.S3_BUCKET,
+    prefix: 'tmp'
+  }
 }
 
 const scenarios = [
@@ -45,13 +45,14 @@ const scenarios = [
       transform: {
         omit: [ 'thumbnail', 'thumbnail_width', 'thumbnail_height', 'href' ]
       },
-      documents: 36273  
+      objects: 36273
     },
     export: {
       query: { $and: [{ year: { $gte: 1970 } }, { year: { $lt: 2000 } }] },
       transform:{
         omit: [ '_id' ]
       },
+      objects: 6738,
       size: 3385369
     }
   },
@@ -60,13 +61,14 @@ const scenarios = [
     contentType: 'application/geo+json',
     service: 'features',
     import: {
-      documents: 255
+      objects: 255
     },
     export: {
       transform:{
         omit: [ '_id' ]
       },
       chunkSize: 100,
+      objects: 255,
       size: 21365820
     }
   },
@@ -82,13 +84,14 @@ const scenarios = [
           'Number of employees': { asNumber: true }
         }
       },
-      documents: 100000,
+      objects: 100000,
     }, 
     export: {
       transform:{
         omit: [ '_id' ]
       },
       query: { $select: ['Name', 'Industry', 'Founded'] },
+      objects: 100000,
       size: 4329209
     }
   }
@@ -113,19 +116,19 @@ function runTests (scenario) {
   })
     .timeout(120000)
   it(`[${scenario.name}] import input dataset`, async () => {
-    // TODO
-    await service.create({
+    const response = await service.create({
       method: 'import',
       id: inputId,
       servicePath: scenario.service,
       transform: scenario.import.transform
     })
+    expect(response.objects).to.equal(scenario.import.objects)
   })
     .timeout(120000)
   it(`[${scenario.name}] check imported collection`, async () => {
     const service = app.service(scenario.service)
     const response = await service.find()
-    expect(response.total).to.equal(scenario.import.documents)
+    expect(response.total).to.equal(scenario.import.objects)
   })
   it(`[${scenario.name}] clean input dataset`, async () => {
     const response = await s3Service.remove(inputId)
@@ -141,6 +144,7 @@ function runTests (scenario) {
       format: scenario.name,
       chunkSize: scenario.export.chunkSize
     })
+    expect(response.objects).to.equal(scenario.export.objects)
     expect(response.id).toExist()
     outputIds.push(response.id)
   })
@@ -206,7 +210,7 @@ describe('feathers-import-export', () => {
       expect(app.service(scenario.service)).toExist()
     }
     // create s3 service
-    app.use('path-to-s3', new S3Service(s3Options), {
+    app.use('path-to-s3', new S3Service(options.s3Options), {
       methods: ['uploadFile', 'downloadFile']
     })
     s3Service = app.service('path-to-s3')
