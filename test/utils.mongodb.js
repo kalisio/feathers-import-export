ed8ec7201ca8f402ff4256e4e134c68ea9c0081e
cnouguier <ch.nouguier@gmail.com>
2023-11-04 01:15:04 +0100

    chore: ensure find pagination uses chunksize

--- test/utils.mongodb.js
+++ test/utils.mongodb.js
@@ -6,7 +6,7 @@ const { MongoClient } = mongodb
 let client
 let database
 
-export async function createMongoService (name, paginate = { default: 10, max: 500 }) {
+export async function createMongoService (name, paginate = { default: 10, max: 50 }) {
   client = new MongoClient('mongodb://127.0.0.1:27017')
   await client.connect()
   database = client.db('feathers-import-export')
