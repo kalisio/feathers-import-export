f022cb7405c47c9dc6e8a877aab9b5e53fd3ef77
tristan-greffe <tristan.greffe@hotmail.com>
2025-12-16 20:13:33 +0100

    chore: implement SonarQube analysis and update CI workflow [additional tests]

--- scripts/init_runner.sh
+++ scripts/init_runner.sh
@@ -17,7 +17,7 @@ init_github_run_tests() {
 }
 
 init_github_additional_tests() {
-    install_reqs age sops nvm node18 node22 mongo6 mongo7
+    install_reqs age sops nvm node20 node22 mongo7 mongo8
     sudo apt-get install gdal-bin
 }
 
